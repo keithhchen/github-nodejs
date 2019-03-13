93202baa046c9f179516bf079d86200730ff026b
Keith Chen <keith.chenzhun@yahoo.com>
2019-03-13 17:44:42 -0400

    Abstracted api calls to /api

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-const request = require('./request')
+const api = require("./api")
 
-async function editTopics(repo, names) {
-  if (!repo || !names) {
-    throw new Error("Params missing.")
+async function addTopic(topic, include = "") {
+  if (!topic) {
+    throw new Error("No topic name provided.")
   }
-
-  try {
-    const url = `https://api.github.com/repos/${repo}/topics`
-    const body = {
-      names
+  const repos = await api.getOrgRepos('Berklee-Game-Audio')
+  repos.forEach(repo => {
+    if (!repo.topics.includes(topic) && repo.full_name.toLowerCase().includes(include)) {
+      api.editTopics(repo.full_name, [topic, ...repo.topics])
+        .then(res => console.log(repo.name + ": " + res.names))
+        .catch(err => console.log(err))
     }
-    const res = await request.put({ url, body })
-    return res
-  } catch (err) {
-    throw new Error(err)
-  }
-}
-
-async function getOrgRepos(org) {
-  if (!org) {
-    throw new Error("Params missing.")
-  }
-  try {
-    const url = `https://api.github.com/orgs/${org}/repos`
-    const res = await request.get(url)
-    return res
-  } catch (err) {
-    throw new Error(err)
-  }
+  })
 }
